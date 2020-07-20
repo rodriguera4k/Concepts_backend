5db82ae25c769b8b3e3bd4daf8095d3cb8f6a89d
coimbrox <gabao.coimbra@gmail.com>
2020-07-20 19:15:12 -0300

    Nodemon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const app = express();
 
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+  return response.json({ message: 'Hello World xD' });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Back-end Iniciado');
+});
