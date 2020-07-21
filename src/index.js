f2c57479dba4eddf0cab17d334d67d6fd0d022ad
coimbrox <gabao.coimbra@gmail.com>
2020-07-21 02:04:55 -0300

    Utilização de Paramêtros

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -10,6 +18,11 @@ app.get('/projects', (request, response) => {
 })
 
 app.post('/projects', (request, response) => {
+  const { title, name } = request.body;
+
+  console.log(title);
+  console.log(name);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +31,10 @@ app.post('/projects', (request, response) => {
 });
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+
+  console.log(id);
+
   return response.json([
     'Projeto 777',
     'Projeto 2',
@@ -34,5 +51,5 @@ app.delete('/projects/:id', (request, response) => {
 
 
 app.listen(3333, () => {
-  console.log('Back-end Iniciado');
+  console.log('🚀 Back-end Iniciado');
 });
