3ee74dab2b14e1e741f8214b863e751831ddac4a
coimbrox <gabao.coimbra@gmail.com>
2020-07-21 01:24:13 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ const express = require('express');
 
 const app = express();
 
+app.get('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+  ]);
+})
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World xD' });
+app.post('/projects', (request, response) => {
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
 });
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 777',
+    'Projeto 2',
+    'Projeto 3',
+  ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+  return response.json([
+    'Projeto 777',
+    'Projeto 2',
+  ]);
+});
+
 
 app.listen(3333, () => {
   console.log('Back-end Iniciado');
